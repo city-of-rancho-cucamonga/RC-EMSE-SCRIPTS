58a70fb239bcd7d86143f821a5dcc3251c5f568c
grayquarter <john@grayquarter.com>
2019-05-31 10:02:53 -0700

    Removing identical custom functions

--- INCLUDES_CUSTOM/getACARecordParam4Notification.js
+++
@@ -1,7 +0,0 @@
-function getACARecordParam4Notification(params, acaUrl) {
-	// pass in a hashtable and it will add the additional parameters to the table
-
-	addParameter(params, "$$acaRecordUrl$$", getACARecordURL(acaUrl));
-
-	return params;
-}
