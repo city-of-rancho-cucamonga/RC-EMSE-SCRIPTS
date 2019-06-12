7cae0f0850d486782e8df291a8c6aaf72ef9872a
grayquarter <john@grayquarter.com>
2019-06-12 11:43:44 -0700

    Update 1globals.js

--- INCLUDES_CUSTOM/1globals.js
+++ INCLUDES_CUSTOM/1globals.js
@@ -1,7 +1,7 @@
 var SLACKURL = "https://hooks.slack.com/services/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";  // from globals
 	
 if (matches(currentUserID,'JSCHOMP','SID.SIPHOMSAY','MICHELLE.FERNANDES')) {
-	showDebug = 1;
+	showDebug = true;
 	}
 
 	
