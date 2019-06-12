1b0cd3ad4ea68075c501b837743eef375c8eedec
grayquarter <john@grayquarter.com>
2019-06-12 11:43:15 -0700

    updated debugging list

--- INCLUDES_CUSTOM/1globals.js
+++ INCLUDES_CUSTOM/1globals.js
@@ -1,6 +1,6 @@
 var SLACKURL = "https://hooks.slack.com/services/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";  // from globals
 	
-if (matches(currentUserID,'SID.SIPHOMSAY','MICHELLE.FERNANDES')) {
+if (matches(currentUserID,'JSCHOMP','SID.SIPHOMSAY','MICHELLE.FERNANDES')) {
 	showDebug = 1;
 	}
 
