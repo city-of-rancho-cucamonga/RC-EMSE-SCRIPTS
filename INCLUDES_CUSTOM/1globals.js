92e18ee0ab12542653aa0068b83ba373283716a7
grayquarter <john@grayquarter.com>
2019-07-01 15:09:51 -0700

    useAppSpecificGroupName to true

--- INCLUDES_CUSTOM/1globals.js
+++ INCLUDES_CUSTOM/1globals.js
@@ -1,6 +1,8 @@
 var SLACKURL = "https://hooks.slack.com/services/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";  // from globals
 var ENVIRON = "SUPP";
 	
+useAppSpecificGroupName = true;
+
 if (matches(currentUserID,'JSCHOMP','SID.SIPHOMSAY','MICHELLE.FERNANDES')) {
 	showDebug = true;
 	}
