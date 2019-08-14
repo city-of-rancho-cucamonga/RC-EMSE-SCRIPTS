b322ad9195081642d03ccb31b8105b7e6d479bfb
grayquarter <john@grayquarter.com>
2019-08-14 10:36:33 -0700

    load asi with group name
     load ASI again using group name for backward compatibility.   Have to put this
     in custom area since we can't edit the new INCLUDES_ACCELA_GLOBALS

--- INCLUDES_CUSTOM/1globals.js
+++ INCLUDES_CUSTOM/1globals.js
@@ -2,6 +2,9 @@ var SLACKURL = "https://hooks.slack.com/services/TJ6R4KUM8/BJU7Q2J7N/IpWTQGafUNO
 var ENVIRON = "SUPP";
 	
 useAppSpecificGroupName = true;
+// load ASI again using group name for backward compatibility.   Have to put this
+// in custom area since we can't edit the new INCLUDES_ACCELA_GLOBALS
+loadAppSpecific(AInfo); 
 
 if (matches(currentUserID,'JSCHOMP','SID.SIPHOMSAY','MICHELLE.FERNANDES')) {
 	showDebug = true;
