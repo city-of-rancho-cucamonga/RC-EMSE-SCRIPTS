b75dc9005e7517ebf3166f36787c10eb8deefb61
RCEMSE <50383906+RCEMSE@users.noreply.github.com>
2019-08-22 17:26:39 -0700

    Update bldSepticFee.js

--- INCLUDES_CUSTOM/bldSepticFee.js
+++ INCLUDES_CUSTOM/bldSepticFee.js
@@ -33,7 +33,8 @@ function bldSepticFee() {
 	}
 	//end replaced branch: BuildingSepticFee:Lookup;
 	if (newSepticTotal > 0) {
-		updateFee('SE001', 'BLD_SEPTIC', 'FINAL', newSepticTotal, 'N');
+		updateFee('SE001', 'BLD_SEPTIC', 'FINAL', newSepticTotal*1.026, 'N');
+		updateFee('SE002', 'BLD_SEPTIC', 'FINAL', newSepticTotal*1.026, 'N');
 		updateFee('SE006', 'BLD_SEPTIC', 'FINAL', newSepticTotal, 'N');
 		updateFee('SE009', 'BLD_SEPTIC', 'FINAL', newSepticTotal, 'N');
 	}
