26c4cb18aa3cebbcc739b4c8c162b32beeeff26f
RCEMSE <50383906+RCEMSE@users.noreply.github.com>
2019-09-17 15:31:12 -0700

    Update IRSA;BUILDING!TENANT IMPROVEMENT!NA!NA.js

--- Scripts/Event/IRSA;BUILDING!TENANT IMPROVEMENT!NA!NA.js
+++ Scripts/Event/IRSA;BUILDING!TENANT IMPROVEMENT!NA!NA.js
@@ -1,6 +1,6 @@
 
 if (inspType == 'Final Inspection' && inspResult == 'Approved' && AInfo['FIRE SITE INFO RECORD.Operational Permit Required'] == 'Yes') {
-	email('shane.adams@cityofrc.us,jennifer.quan@cityofrc.us,sid.siphomsay@cityofrc.us,tammie.graciano@cityofrc.us', 'noreply@accela.com', 'Operational Permit Required for ' + capIDString + '', 'Final Inspection has been approved for Record# ' + capIDString + '. Please respond accordingly');
+	email('sid.siphomsay@cityofrc.us', 'noreply@accela.com', 'Operational Permit Required for ' + capIDString + '', 'Final Inspection has been approved for Record# ' + capIDString + '. Please respond accordingly');
 }
 
 if (inspType == 'Fire Final' && matches(inspResult, 'Approved') && AInfo['FIRE SITE INFO RECORD.Fire Site Info Record'] != null) {
