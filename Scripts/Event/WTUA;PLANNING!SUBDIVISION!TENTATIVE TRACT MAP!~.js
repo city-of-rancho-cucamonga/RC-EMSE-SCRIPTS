5ff20ec17efcc4548fccf40757ca6f752d95466d
RCEMSE <RCEMSE@users.noreply.github.com>
2019-12-04 10:24:12 -0800

    Update WTUA;PLANNING!SUBDIVISION!TENTATIVE TRACT MAP!~.js

--- Scripts/Event/WTUA;PLANNING!SUBDIVISION!TENTATIVE TRACT MAP!~.js
+++ Scripts/Event/WTUA;PLANNING!SUBDIVISION!TENTATIVE TRACT MAP!~.js
@@ -5,7 +5,7 @@ if (wfTask == 'Planning Commission' && wfStatus == 'Approved') {
 		rcUserAssigned = capDetail.getAsgnStaff();
 	else
 		rcUserAssigned = '(A user has not been assigned)';
-	email('Rafael.Balneg@cityofrc.us,Jennifer.Nakamura@cityofrc.us,jose.ramirez@cityofrc.us', 'noreply@cityofrc.us', 'A Project (' + capIDString + ') Has Been Approved', 'Project ' + capIDString + ' has been approved and may require an update by GIS. Please review it and make any necessary updates. If you have any questions, please contact the assigned staff, ' + rcUserAssigned + '.');
+	email('sid.siphomsay@cityofrc.us,Jennifer.Nakamura@cityofrc.us,dat.tran@cityofrc.us', 'noreply@cityofrc.us', 'A Project (' + capIDString + ') Has Been Approved', 'Project ' + capIDString + ' has been approved and may require an update by GIS. Please review it and make any necessary updates. If you have any questions, please contact the assigned staff, ' + rcUserAssigned + '.');
 }
 
 if (wfTask == 'Planning Commission' && wfStatus == 'Approved') {
