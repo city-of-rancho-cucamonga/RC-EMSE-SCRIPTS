803547830c5af874b24f0b2f62d7d6869254169f
RCEMSE <RCEMSE@users.noreply.github.com>
2020-01-14 16:39:28 -0800

    Update WTUA;ENGINEERING!STR!NA!NA.js

--- Scripts/Event/WTUA;ENGINEERING!STR!NA!NA.js
+++ Scripts/Event/WTUA;ENGINEERING!STR!NA!NA.js
@@ -4,3 +4,10 @@ if (wfTask == 'Application Submittal' && wfStatus == 'Invoiced Fees')
 	priAddrRec = priAddressOnCap();
 	email('Sid.Siphomsay@cityofrc.us', 'AA-NoReply@cityofrc.us', 'Invoiced Fee has been applied on: ' + capIDString, 'The primary address on this permit is ' + priAddrRec + '. Please review it and respond accordingly.');
 }
+
+if (wfTask == 'Closed' && wfStatus == 'Final') 
+
+{
+	priAddrRec = priAddressOnCap();
+	email('Sid.Siphomsay@cityofrc.us', 'AA-NoReply@cityofrc.us', 'The Street Tree Removal and Replacement Application is closed on: ' + capIDString, 'The primary address on this permit is ' + priAddrRec + '. Please review it and respond accordingly.');
+}
