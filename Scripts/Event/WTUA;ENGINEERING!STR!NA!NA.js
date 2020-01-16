5c4911e4a3db0b61985ab86486a318aac4cbbabd
RCEMSE <50383906+RCEMSE@users.noreply.github.com>
2020-01-15 17:48:49 -0800

    Update WTUA;ENGINEERING!STR!NA!NA.js

--- Scripts/Event/WTUA;ENGINEERING!STR!NA!NA.js
+++ Scripts/Event/WTUA;ENGINEERING!STR!NA!NA.js
@@ -22,3 +22,19 @@ if (wfTask == 'Closed' && wfStatus == 'Final')
 	priAddrRec = priAddressOnCap();
 	email('Sid.Siphomsay@cityofrc.us', 'AA-NoReply@cityofrc.us', 'The Street Tree Removal and Replacement Application is closed on: ' + capIDString, 'The primary address on this permit is ' + priAddrRec + '. Please review it and respond accordingly.');
 }
+
+if (wfTask == 'PW Action' && wfStatus == 'Issued') {
+	editAppSpecific('KEY DATES.Permit Issued', wfDateMMDDYYYY);
+}
+
+if (wfTask == 'PW Action' && wfStatus == 'Issued' && AInfo['Expire Date'] != undefined && AInfo['Expire Date'] != '') {
+	editAppSpecific('KEY DATES.Permit Expires', AInfo['Expire Date']);
+}
+
+if (wfTask == 'Closed' && wfStatus == 'Final') {
+	editAppSpecific('KEY DATES.Final', wfDateMMDDYYYY);
+}
+
+if (wfTask == 'Permit Extension' && wfStatus == 'Approved') {
+	editAppSpecific('KEY DATES.Permit Expires', AInfo['New Expiration Date']);
+}
