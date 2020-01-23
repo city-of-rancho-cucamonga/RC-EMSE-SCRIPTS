322d287b6b2a7178508b72201c4ba0df2f999089
RCEMSE <50383906+RCEMSE@users.noreply.github.com>
2020-01-23 14:44:33 -0800

    Update WTUA;ENGINEERING!STR!NA!NA.js

--- Scripts/Event/WTUA;ENGINEERING!STR!NA!NA.js
+++ Scripts/Event/WTUA;ENGINEERING!STR!NA!NA.js
@@ -1,8 +1,17 @@
+if (wfTask == 'Application Submittal' && wfStatus == 'Accepted') 
+{
+	priAddrRec = priAddressOnCap();
+	email('Sid.Siphomsay@cityofrc.us', 'AA-NoReply@cityofrc.us', 'Invoiced Fee has been applied on: ' + capIDString, 'The primary address on this permit is ' + priAddrRec + '. Please review it and respond accordingly.');
+}
+
+
 if (wfTask == 'Application Submittal' && wfStatus == 'Invoiced Fees') 
 {
 	priAddrRec = priAddressOnCap();
 	email('Sid.Siphomsay@cityofrc.us', 'AA-NoReply@cityofrc.us', 'Invoiced Fee has been applied on: ' + capIDString, 'The primary address on this permit is ' + priAddrRec + '. Please review it and respond accordingly.');
 }
+
+
 
 if (wfTask == 'PW Public Outreach' && wfStatus == 'Invoiced Fees') 
 {
