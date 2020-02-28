99b9ef0ad7f8771a5c03da68a70a2d790fea36a4
Sid Siphomsay <sid.siphomsay@cityofrc.us>
2020-02-27 16:32:29 -0800

    Create ASIUA;BUILDING!RESIDENTIAL!MISCELLANEOUS!SWIMMING POOL.js

---
+++ Scripts/Event/ASIUA;BUILDING!RESIDENTIAL!MISCELLANEOUS!SWIMMING POOL.js
@@ -0,0 +1,9 @@
+if (typeof(TYPESOFWORK) == 'object') {
+	//replaced branch(BLDEMSEFee:MiscFee)
+	bldMiscFee();
+}
+
+if (typeof(ELECTRICALINFORMATIONTABLE) == 'object') {
+	//replaced branch(BLDEMSEFee:ElectricalFeeNoSplit)
+	bldElecFeeNoSplit();
+}
