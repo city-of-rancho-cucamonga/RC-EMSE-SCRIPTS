6685ad1302eab1f8a2c5bf4d56ddcb99933f5f1b
Sid Siphomsay <sid.siphomsay@cityofrc.us>
2020-03-09 16:23:58 -0700

    Update ISA;BUILDING!COMMERCIAL!NEW CONSTRUCTION!NA.js

--- Scripts/Event/ISA;BUILDING!COMMERCIAL!NEW CONSTRUCTION!NA.js
+++ Scripts/Event/ISA;BUILDING!COMMERCIAL!NEW CONSTRUCTION!NA.js
@@ -4,3 +4,8 @@ if (inspType == 'Temporary Power') {
 	insertSubProcess('Utility Meter Release', 'BLD_NEWCONINSP_UTILITYMTRREL', true);
 	insertSubProcess('Gas Meter Release', 'BLD_NEWCONINSP_GASMTRREL', true);
 }
+
+
+if (inspType == 'Drywall, Nailing, Interior Lath' && inspStatus == 'Scheduled') {
+	email('sid.siphomsay@cityofrc.us,ssiphomsay@yahoo.com', 'noreply@accela.com', 'Drywall, Nailing, Interior Lath inspection notification for ' + capIDString + '-' +priAddrRec, 'Drywall, Nailing, Interior Lath inspection has been scheduled for Record# ' + capIDString + '. Please respond accordingly');
+}
