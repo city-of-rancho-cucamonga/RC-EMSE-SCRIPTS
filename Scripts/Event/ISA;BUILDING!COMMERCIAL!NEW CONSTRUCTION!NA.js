30962ba6ec9bad57ec6c007cfeeb68ab0ec36b2a
Sid Siphomsay <sid.siphomsay@cityofrc.us>
2020-03-09 17:48:41 -0700

    Update ISA;BUILDING!COMMERCIAL!NEW CONSTRUCTION!NA.js

--- Scripts/Event/ISA;BUILDING!COMMERCIAL!NEW CONSTRUCTION!NA.js
+++ Scripts/Event/ISA;BUILDING!COMMERCIAL!NEW CONSTRUCTION!NA.js
@@ -7,5 +7,5 @@ if (inspType == 'Temporary Power') {
 
 if (inspType == 'Drywall, Nailing, Interior Lath') {
 	priAddrRec = priAddressOnCap();
-	email('sid.siphomsay@cityofrc.us', 'noreply@accela.com', 'Notification for ' + capIDString + '-' + priAddrRec ,   'Dry Wall Inspection has been scheduled for Record# ' + capIDString + ' at ' + priAddrRec +'. Please respond accordingly');
+	email('sid.siphomsay@cityofrc.us', 'noreply@accela.com', 'Drywall, Nailing, Interior Lath Inspection Scheduled for ' + capIDString + ' at ' + priAddrRec ,   'Drywall, Nailing, Interior Lath inspection has been scheduled for Record# ' + capIDString + ' at ' + priAddrRec +'. Please respond accordingly');
 }
