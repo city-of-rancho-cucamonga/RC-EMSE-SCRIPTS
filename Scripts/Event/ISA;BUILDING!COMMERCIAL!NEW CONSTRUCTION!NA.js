1ca530df0a9bf9249656ab21b293fc4c5c44830e
Sid Siphomsay <sid.siphomsay@cityofrc.us>
2020-03-10 10:06:06 -0700

    Update ISA;BUILDING!COMMERCIAL!NEW CONSTRUCTION!NA.js

--- Scripts/Event/ISA;BUILDING!COMMERCIAL!NEW CONSTRUCTION!NA.js
+++ Scripts/Event/ISA;BUILDING!COMMERCIAL!NEW CONSTRUCTION!NA.js
@@ -5,6 +5,8 @@ if (inspType == 'Temporary Power') {
 	insertSubProcess('Gas Meter Release', 'BLD_NEWCONINSP_GASMTRREL', true);
 }
 
+
+inspStatus = aa.inspection.getInspectionStatus();
 if (inspType == 'Drywall, Nailing, Interior Lath' && inspStatus == 'Scheduled') {
 	priAddrRec = priAddressOnCap();
 	email('sid.siphomsay@cityofrc.us', 'noreply@accela.com', 'Dry wall Inspection at' + priAddrRec, 'Dry Wall Inspection has been scheduled for Record# ' + capIDString + ' at ' + priAddrRec +'. Please respond accordingly');
