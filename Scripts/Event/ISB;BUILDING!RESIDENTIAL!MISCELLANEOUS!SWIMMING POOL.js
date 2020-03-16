51d0971eaab1662f106a3c7c2b9dafe40b9e352d
Sid Siphomsay <sid.siphomsay@cityofrc.us>
2020-03-16 11:47:04 -0700

    Update ISB;BUILDING!RESIDENTIAL!MISCELLANEOUS!SWIMMING POOL.js

--- Scripts/Event/ISB;BUILDING!RESIDENTIAL!MISCELLANEOUS!SWIMMING POOL.js
+++ Scripts/Event/ISB;BUILDING!RESIDENTIAL!MISCELLANEOUS!SWIMMING POOL.js
@@ -1,5 +1,16 @@
-if (!matches(inspType, 'Pre-Inspection') && capStatus == "Pre-Inspection") {
+if (!publicUser && !matches(capStatus, 'Pre-Inspection', 'Issued', 'Inspections', 'Inspection Phase') && !(matches(appTypeArray[1], 'Grading', 'Code Violation') || matches(appTypeArray[2], 'Septic'))) {
+	cancel = true;
+	showMessage = true;
+	comment('The inspection cannot be scheduled because the permit does not have a record status which allows inspection scheduling');
+}
+
+
+
+if (!matches(inspType, 'Bond Beam') && capStatus == "Pre-Inspection") {
 	cancel = true;
 	showMessage = true;
 	comment('Final inspection cannot be scheduled after Transportaion Fees are paid.');
 }
+
+
+
