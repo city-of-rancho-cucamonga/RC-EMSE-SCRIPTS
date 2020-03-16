3c809939c5c0fa8ad8d80d936af44a8d380f22d4
Sid Siphomsay <sid.siphomsay@cityofrc.us>
2020-03-16 10:44:14 -0700

    Create ISB;BUILDING!RESIDENTIAL!MISCELLANEOUS!SWIMMING POOL.js

---
+++ Scripts/Event/ISB;BUILDING!RESIDENTIAL!MISCELLANEOUS!SWIMMING POOL.js
@@ -0,0 +1,9 @@
+//
+
+if (matches(capStatus,'Pre-Inspection') && !matches(insType, 'Pre-Inspection'))  
+	
+{
+	cancel = true;
+	showMessage = true;
+	comment('The inspection cannot be scheduled because the permit does not have a record status which allows inspection scheduling');
+}
