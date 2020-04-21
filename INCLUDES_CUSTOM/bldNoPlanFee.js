dcc65fdc73f63ca91a0fc13b25a7dda1aab8c8f6
Sid Siphomsay <sid.siphomsay@cityofrc.us>
2020-04-20 17:19:25 -0700

    Update bldNoPlanFee.js

--- INCLUDES_CUSTOM/bldNoPlanFee.js
+++ INCLUDES_CUSTOM/bldNoPlanFee.js
@@ -22,11 +22,32 @@ function bldNoPlanFee() {
 					newNPTotal += MECHANICALINFORMATION[xxx]['Quantity'] * 131.00;
 		}
 
-		if (typeof(MECHANICALINFORMATION) == 'object') {
-			for (xxx in MECHANICALINFORMATION)
-				if (MECHANICALINFORMATION[xxx]['Mechanical Type'] == 'HVAC W/Ductwork (Replacement Only/Same Location)')
-					newNPTotal += MECHANICALINFORMATION[xxx]['Quantity'] * 262.00;
+		
+		//Need to modify
+		//if (typeof(MECHANICALINFORMATION) == 'object') {
+			//for (xxx in MECHANICALINFORMATION)
+			//	if (MECHANICALINFORMATION[xxx]['Mechanical Type'] == 'HVAC W/Ductwork (Replacement Only/Same Location)')
+			//		newNPTotal += MECHANICALINFORMATION[xxx]['Quantity'] * 262.00;
+		//}
+
+		//Modified from above
+		if (typeof(MECHANICALINFORMATIONTABLE) == 'object') {
+		for (xxx in MECHANICALINFORMATIONTABLE)
+			if (MECHANICALINFORMATIONTABLE[xxx]['Mechanical Type'] == 'HVAC W/Ductwork (Replacement Only/Same Location)' && parseFloat(MECHANICALINFORMATIONTABLE[xxx]['Quantity']) > 0)
+				newNPTotal += 262.00;
+		comment('What is newNPTotal = ' + newNPTotal);
 		}
+
+		if (typeof(MECHANICALINFORMATIONTABLE) == 'object') {
+		for (xxx in MECHANICALINFORMATIONTABLE)
+			if (MECHANICALINFORMATIONTABLE[xxx]['Mechanical Type'] == 'HVAC W/Ductwork (Replacement Only/Same Location)' && parseFloat(MECHANICALINFORMATIONTABLE[xxx]['Quantity']) > 1)
+				newNPTotal += (MECHANICALINFORMATIONTABLE[xxx]['Quantity'] - 1) * 51;
+		comment('What is newNPTotal = ' + newNPTotal);
+		}
+
+
+
+
 		
 		if (typeof(MECHANICALINFORMATION) == 'object') {
 			for (xxx in MECHANICALINFORMATION)
