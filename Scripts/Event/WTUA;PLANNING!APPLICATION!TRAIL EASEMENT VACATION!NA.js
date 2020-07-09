baf872b5b3a31808d15df83c5f404566b309a1a7
Sid Siphomsay <sid.siphomsay@cityofrc.us>
2020-07-08 18:57:37 -0700

    Update WTUA;PLANNING!APPLICATION!TRAIL EASEMENT VACATION!NA.js

--- Scripts/Event/WTUA;PLANNING!APPLICATION!TRAIL EASEMENT VACATION!NA.js
+++ Scripts/Event/WTUA;PLANNING!APPLICATION!TRAIL EASEMENT VACATION!NA.js
@@ -5,5 +5,5 @@ if (wfTask == 'City Council' && wfStatus == 'Approved') {
 		rcUserAssigned = capDetail.getAsgnStaff();
 	else
 		rcUserAssigned = '(A user has not been assigned)';
-	email('Rafael.Balneg@cityofrc.us,Jennifer.Nakamura@cityofrc.us,jose.ramirez@cityofrc.us', 'noreply@cityofrc.us', 'A Project (' + capIDString + ') Has Been Approved', 'Project ' + capIDString + ' has been approved and may require an update by GIS. Please review it and make any necessary updates. If you have any questions, please contact the assigned staff, ' + rcUserAssigned + '.');
+	email('steve.lawdis@cityofrc.us,Jennifer.Nakamura@cityofrc.us,jose.ramirez@cityofrc.us', 'noreply@accela.com', 'A Project (' + capIDString + ') Has Been Approved', 'Project ' + capIDString + ' has been approved and may require an update by GIS. Please review it and make any necessary updates. If you have any questions, please contact the assigned staff, ' + rcUserAssigned + '.');
 }
