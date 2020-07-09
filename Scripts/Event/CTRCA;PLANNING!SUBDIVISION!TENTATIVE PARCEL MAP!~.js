a857fbd286336dd8330686b972d97d90f25e60dc
Sid Siphomsay <sid.siphomsay@cityofrc.us>
2020-07-09 12:18:01 -0700

    Update CTRCA;PLANNING!SUBDIVISION!TENTATIVE PARCEL MAP!~.js

--- Scripts/Event/CTRCA;PLANNING!SUBDIVISION!TENTATIVE PARCEL MAP!~.js
+++ Scripts/Event/CTRCA;PLANNING!SUBDIVISION!TENTATIVE PARCEL MAP!~.js
@@ -4,4 +4,4 @@ if (AInfo['GENERAL INFO.Parcel Map Number'] != null) {
 	var updateResut = aa.cap.updateCapAltID(capId, newAltID);
 }
 
-email('sid.siphomsay@cityofrc.us', 'noreply@cityofrc.us', cap.getCapType().getAlias().toUpperCase() + ' - a new application was submitted via Accelerate: ' + newAltID, 'A(n) ' + cap.getCapType().getAlias() + ' application (' + newAltID + ') was submitted via Accelerate. Please review it and proceed as appropriate.');
+email('sid.siphomsay@cityofrc.us', 'noreply@accela.com', cap.getCapType().getAlias().toUpperCase() + ' - a new application was submitted via Accelerate: ' + newAltID, 'A(n) ' + cap.getCapType().getAlias() + ' application (' + newAltID + ') was submitted via Accelerate. Please review it and proceed as appropriate.');
