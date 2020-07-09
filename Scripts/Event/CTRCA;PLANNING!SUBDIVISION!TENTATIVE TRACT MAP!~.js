4650b85133c51d7f44b087d764b983b768337a4c
Sid Siphomsay <sid.siphomsay@cityofrc.us>
2020-07-09 12:17:57 -0700

    Update CTRCA;PLANNING!SUBDIVISION!TENTATIVE TRACT MAP!~.js

--- Scripts/Event/CTRCA;PLANNING!SUBDIVISION!TENTATIVE TRACT MAP!~.js
+++ Scripts/Event/CTRCA;PLANNING!SUBDIVISION!TENTATIVE TRACT MAP!~.js
@@ -4,7 +4,7 @@ if (AInfo['GENERAL INFO.Tract Map Number'] != null) {
 	var updateResut = aa.cap.updateCapAltID(capId, newAltID);
 }
 
-email('sid.siphomsay@cityofrc.us', 'noreply@cityofrc.us', cap.getCapType().getAlias().toUpperCase() + ' - a new application was submitted via Accelerate: ' + newAltID, 'A(n) ' + cap.getCapType().getAlias() + ' application (' + newAltID + ') was submitted via Accelerate. Please review it and proceed as appropriate.');
+email('sid.siphomsay@cityofrc.us', 'noreply@accela.com', cap.getCapType().getAlias().toUpperCase() + ' - a new application was submitted via Accelerate: ' + newAltID, 'A(n) ' + cap.getCapType().getAlias() + ' application (' + newAltID + ') was submitted via Accelerate. Please review it and proceed as appropriate.');
 // DISABLED: CTRCA:Planning/Subdivision/Tentative Tract Map/*:25
 // $$altID$$ = newAltID;
 // DISABLED: CTRCA:Planning/Subdivision/Tentative Tract Map/*:30
