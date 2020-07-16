cf0187178c0c29fc7adf8d7d065a7ec655e6dc8a
Sid Siphomsay <sid.siphomsay@cityofrc.us>
2020-07-16 11:57:12 -0700

    Update IRSA;BUILDING!RESIDENTIAL!SEPTIC!NA.js

--- Scripts/Event/IRSA;BUILDING!RESIDENTIAL!SEPTIC!NA.js
+++ Scripts/Event/IRSA;BUILDING!RESIDENTIAL!SEPTIC!NA.js
@@ -1,6 +1,17 @@
 
-if (matches(inspType, 'Final Plumbing') && inspResult == 'Approved' && typeof(SEPTICINFORMATIONDETAIL) == 'object') {
+if (matches(inspType, 'Final Plumbing') && inspResult == 'Approved' && typeof(SEPTICINFORMATIONDETAIL) == 'object') 
+{
 	closeTask('Inspections', 'Final Inspection Complete', 'Closed via script', '');
 	setTask('Closed', 'N', 'Y');
 	updateAppStatus('Finalized', 'Updated via script');
+}
+
+
+
+if (matches(inspType, 'Pre-Inspection') && inspResult == 'Approved') 
+{
+	closeTask('Pre Inspection', 'Completed', 'Closed via script', '');
+	setTask('Closed', 'N', 'Y');
+	setTask('Permit Issuance', 'Y', 'N');
+	updateAppStatus('Ready to Issue', 'Updated via script');
 }
