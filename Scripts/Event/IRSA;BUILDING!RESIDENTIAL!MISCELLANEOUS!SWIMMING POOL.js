a2044a05eb17458dd628e0a26b4f9a6837359284
Sid Siphomsay <sid.siphomsay@cityofrc.us>
2020-07-16 11:38:21 -0700

    Create IRSA;BUILDING!RESIDENTIAL!MISCELLANEOUS!SWIMMING POOL.js

---
+++ Scripts/Event/IRSA;BUILDING!RESIDENTIAL!MISCELLANEOUS!SWIMMING POOL.js
@@ -0,0 +1,9 @@
+if (matches(inspType, 'Pre-Inspection') && inspResult == 'Approved') 
+
+{
+	closeTask('Pre Inspection', 'Completed', 'Closed via script', '');
+	setTask('Closed', 'N', 'Y');
+	setTask('Permit Issuance', 'Y', 'N');
+	updateAppStatus('Ready to Issue', 'Updated via script');
+
+}
