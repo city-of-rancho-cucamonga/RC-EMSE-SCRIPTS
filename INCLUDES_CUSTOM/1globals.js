e9b07213de16fb8276635c5575bfbedc951a92f1
Kyle Wingson <kyle.wingson@cityofrc.us>
2021-01-12 11:35:32 -0800

    Update 1globals.js

--- INCLUDES_CUSTOM/1globals.js
+++ INCLUDES_CUSTOM/1globals.js
@@ -6,7 +6,7 @@ useAppSpecificGroupName = true;
 // in custom area since we can't edit the new INCLUDES_ACCELA_GLOBALS
 loadAppSpecific(AInfo); 
 
-if (matches(currentUserID,'JSCHOMP','SID.SIPHOMSAY','MICHELLE.FERNANDES','LIZA.TUNURI')) {
+if (matches(currentUserID,'JSCHOMP','SID.SIPHOMSAY','MICHELLE.FERNANDES','KYLE.WINGSON','LIZA.TUNURI')) {
 	showDebug = true;
 	}
 
