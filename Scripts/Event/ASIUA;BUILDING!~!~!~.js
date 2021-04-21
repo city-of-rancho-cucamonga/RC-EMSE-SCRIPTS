1ad3a339ce795e86cbb1afa73639c57285679187
Kyle Wingson <kyle.wingson@cityofrc.us>
2021-04-21 08:09:12 -0700

    Update ASIUA;BUILDING!~!~!~.js

--- Scripts/Event/ASIUA;BUILDING!~!~!~.js
+++ Scripts/Event/ASIUA;BUILDING!~!~!~.js
@@ -6,3 +6,19 @@ engDevFeeDrainage();
 // 	br_nch('BLD:EMSEUpdateGISObjects');
 // 	}
 
+
+/////////
+var appCap = aa.cap.getCap(capId).getOutput();
+var appCapType = appCap.getCapType().toString();
+var appCapTypeArr = appCapType.split("/");
+var appT1 = appCapTypeArr[0];
+var appT2 = appCapTypeArr[1];
+var appT3 = appCapTypeArr[2];
+var appT4 = appCapTypeArr[3];
+
+if (!(appT1=='Building' & appT2=='FCS' & appT3=='Miscellaneous' & appT4=='High Pile Storage') & !(appT1=='Building' & appT2=='FCS' & appT3=='Suppression System' & appT4=='Fixed')){
+	bldUpdateGisObjects();
+}
+else{
+	fdBldUpdateGisObjects()
+}
