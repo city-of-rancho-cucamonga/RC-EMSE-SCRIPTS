c58bc47d2c0187fb208e8fe47cf56d62839af1bb
Kyle Wingson <kyle.wingson@cityofrc.us>
2021-04-21 11:30:21 -0700

    Update fdBldUpdateGisObjects.js

--- INCLUDES_CUSTOM/fdBldUpdateGisObjects.js
+++ INCLUDES_CUSTOM/fdBldUpdateGisObjects.js
@@ -2,7 +2,7 @@ function fdBldUpdateGisObjects() {
 
 	var rcFireInspArea = getGISInfo('CITYOFRC', 'Fire Inspection Areas', 'Id');
 	var rcVHFHSZ = getGISInfoArray2('CITYOFRC', 'Very High Fire Severity Zone', 'HAZ_CLASS', -5, 'Feet');
-	var rcFirstDueBoundary = getGISInfoArray2('CITYOFRC', 'First Due Boundary', 'FIRSTDUE_', -5, 'Feet');
+	var rcFirstDueBoundary = getGISInfoArray2('CITYOFRC', 'Fire First Due', 'FIRSTDUE_', -5, 'Feet');
 	var rcIsVacant = getGISInfo2('CITYOFRC', 'Parcels', 'VACANT', -5, 'Feet');
 	if (!rcFireInspArea) {
 		editAppSpecific('GIS INFORMATION.Inspection Area', 'NA');
