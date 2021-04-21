45e1b1a787d7138ebe483fc20dca5210c1f177c1
Kyle Wingson <kyle.wingson@cityofrc.us>
2021-04-20 17:26:45 -0700

    Update ASA;BUILDING!~!~!~.js

--- Scripts/Event/ASA;BUILDING!~!~!~.js
+++ Scripts/Event/ASA;BUILDING!~!~!~.js
@@ -24,3 +24,20 @@ if (lpArray) {
 	//replaced branch(LPUA:Building/*/*/*)
 	licProfUpdateBld();
 }
+
+
+/////////
+var appCap = aa.cap.getCap(capId).getOutput();
+var appCapType = appCap.getCapType().toString();
+var appCapTypeArr = appCapType.split("/");
+var appT1 = appCapTypeArr[0];
+var appT2 = appCapTypeArr[1];
+var appT3 = appCapTypeArr[2];
+var appT4 = appCapTypeArr[3];
+
+if (!(appT1=='Building' & appT2=='FCS' & appT3=='Miscellaneous' & appT4=='High Pile Storage') & !(appT1=='Building' & appT2=='FCS' & appT3=='Suppression System' & appT4=='Fixed')){
+	bldUpdateGisObjects();
+}
+else{
+	fdBldUpdateGisObjects()
+}
